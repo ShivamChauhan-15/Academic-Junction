9d9a70dc2e572b28a5f214cca841d6bf4c849b6b
Shivam Chauhan <142312168+ShivamChauhan-15@users.noreply.github.com>
2024-03-01 19:23:56 +0530

    Footer section updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
         This website contains Subjectwise Notes, CIA and Semester Exam Question Papers of MCA Batch 2022-24 which helps the First Year Students to prepare for their exam and save their time in making notes and searching for all those stuffs. 
-        <span className='credit'>Created by Shivam Chauhan, Harsh Jain and Shivam Kumar.</span>
+        <span className='credit'>Created by Shivam Chauhan and Shivam Kumar.</span>
     </footer>
   )
 }
